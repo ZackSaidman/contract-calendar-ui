d8d8bd0be94eba62d5fe46a6ae4910d3ea511901
Zack Saidman <zachary.saidman@gmail.com>
2025-03-26 21:40:03 -0700

    Make Basic Calendar with Example Events

---
+++ src/components/Calendar.vue
@@ -0,0 +1,33 @@
+<script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        contentHeight: 'auto',
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+        dateClick: this.handleDateClick,
+        events: [
+          { title: 'event 1', date: '2025-03-01' },
+          { title: 'event 2', date: '2025-03-02' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleDateClick: function(arg) {
+      alert('date click! ' + arg.dateStr)
+    }
+  }
+}
+</script>
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
