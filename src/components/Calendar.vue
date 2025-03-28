72c5b8b982152af72d75a6cfe3bfa01ead81d6a2
Zack Saidman <zachary.saidman@gmail.com>
2025-03-27 19:58:30 -0700

    Refactor Calendar Component

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,33 +1,26 @@
-<script>
-import FullCalendar from '@fullcalendar/vue3'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
+<script setup>
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
-export default {
-  components: {
-    FullCalendar // make the <FullCalendar> tag available
-  },
-  data() {
-    return {
-      calendarOptions: {
-        contentHeight: 'auto',
-        plugins: [ dayGridPlugin, interactionPlugin ],
-        initialView: 'dayGridMonth',
-        dateClick: this.handleDateClick,
-        events: [
-          { title: 'event 1', date: '2025-03-01' },
-          { title: 'event 2', date: '2025-03-02' }
-        ]
-      }
-    }
-  },
-  methods: {
-    handleDateClick: function(arg) {
-      alert('date click! ' + arg.dateStr)
-    }
-  }
-}
+// Handle date click event
+const handleDateClick = (arg) => {
+  alert('date click! ' + arg.dateStr);
+};
+
+// Calendar options
+const calendarOptions = {
+  contentHeight: 'auto',
+  plugins: [dayGridPlugin, interactionPlugin],
+  initialView: 'dayGridMonth',
+  dateClick: handleDateClick, // Pass the function reference
+  events: [
+    { title: 'event 1', date: '2025-03-01' },
+    { title: 'event 2', date: '2025-03-02' }
+  ]
+};
 </script>
+
 <template>
   <FullCalendar :options="calendarOptions" />
 </template>
